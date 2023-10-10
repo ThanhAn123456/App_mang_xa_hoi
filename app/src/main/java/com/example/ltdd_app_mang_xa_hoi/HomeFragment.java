1ae06879bf5d4578afef86a84dbc05105a4e87cb
gamotapcode <dinhyenphan@gmail.com>
2023-10-10 17:31:08 +0700

    hoàn thành trang login,sign up, forgetpassword,bạn bè, nhóm tạo fragment cho trang chức năng(Tạo bài viết,..)

--- app/src/main/java/com/example/ltdd_app_mang_xa_hoi/HomeFragment.java
+++ app/src/main/java/com/example/ltdd_app_mang_xa_hoi/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.ltdd_app_mang_xa_hoi;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+        TextView textView = view.findViewById(R.id.taobaiviet);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(requireContext(), ActivityContainerActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/ltdd_app_mang_xa_hoi/MainContainerActivity.java
+++ app/src/main/java/com/example/ltdd_app_mang_xa_hoi/MainContainerActivity.java
@@ -6,6 +6,7 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -14,6 +15,7 @@ import Adapters.ViewPagerAdapter;
 
 public class MainContainerActivity extends AppCompatActivity {
     public ViewPager2 mViewPager;
+    public TextView mTextView;
     public BottomNavigationView mBottomNavigationView;
 
     @Override
@@ -21,6 +23,7 @@ public class MainContainerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_container);
         mViewPager = findViewById(R.id.view_pager);
+        mTextView = findViewById(R.id.title_header);
         mBottomNavigationView = findViewById(R.id.bottom_nav);
         ViewPagerAdapter adapter = new ViewPagerAdapter(this);
         mViewPager.setAdapter(adapter);
@@ -31,18 +34,23 @@ public class MainContainerActivity extends AppCompatActivity {
                 switch (position) {
                     case 0:
                         mBottomNavigationView.getMenu().findItem(R.id.action_home).setChecked(true);
+                        mTextView.setText(getString(R.string.news));
                         break;
                     case 1:
                         mBottomNavigationView.getMenu().findItem(R.id.action_friend).setChecked(true);
+                        mTextView.setText(getString(R.string.sub));
                         break;
                     case 2:
                         mBottomNavigationView.getMenu().findItem(R.id.action_chat).setChecked(true);
+                        mTextView.setText(getString(R.string.chat));
                         break;
                     case 3:
                         mBottomNavigationView.getMenu().findItem(R.id.action_notifications).setChecked(true);
+                        mTextView.setText(getString(R.string.notification));
                         break;
                     case 4:
                         mBottomNavigationView.getMenu().findItem(R.id.action_account).setChecked(true);
+                        mTextView.setText(getString(R.string.profile));
                         break;
 
                 }
@@ -55,15 +63,30 @@ public class MainContainerActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int itemId = item.getItemId();
                 if (itemId == R.id.action_home)
+                {
                     mViewPager.setCurrentItem(0);
+                    mTextView.setText(getString(R.string.news));
+                }
                 if (itemId == R.id.action_friend)
+                {
                     mViewPager.setCurrentItem(1);
+                    mTextView.setText(getString(R.string.sub));
+                }
                 if (itemId == R.id.action_chat)
+                {
                     mViewPager.setCurrentItem(2);
+                    mTextView.setText(getString(R.string.chat));
+                }
                 if (itemId == R.id.action_notifications)
+                {
                     mViewPager.setCurrentItem(3);
+                    mTextView.setText(getString(R.string.notification));
+                }
                 if (itemId == R.id.action_account)
+                {
                     mViewPager.setCurrentItem(4);
+                    mTextView.setText(getString(R.string.profile));
+                }
                 return false;
             }
         });
