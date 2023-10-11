4786d3a8d2dd1e9cb40824ef7a548ff71e3f42e3
ThanhAn123456 <vacnguyen16092003@gmail.com>
2023-10-12 01:56:30 +0700

    tạo Home.java,Home_Adapter, hoàn thành giao diện home

--- app/src/main/java/com/example/ltdd_app_mang_xa_hoi/HomeFragment.java
+++ app/src/main/java/com/example/ltdd_app_mang_xa_hoi/HomeFragment.java
@@ -8,21 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import Adapters.Friend_GroupAdapter;
+import Adapters.Home_Adapter;
+import Dto.Friend_Group;
+import Dto.Home;
+
 public class HomeFragment extends Fragment {
+    ListView listview;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_home, container, false);
-        TextView textView = view.findViewById(R.id.taobaiviet);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(requireContext(), ActivityContainerActivity.class);
-                startActivity(intent);
-            }
-        });
+        listview = view.findViewById(R.id.listviewhome);
+        ArrayList<Home> arrayList = new ArrayList<>();
+        arrayList.add(new Home(R.drawable.avatar, "Nguyen Thanh An","Hôm qua lúc 7:00",R.drawable.img_content));
+        arrayList.add(new Home(R.drawable.avatar, "Nguyen Thanh An","Hôm qua lúc 7:00",R.drawable.img_content));
+        arrayList.add(new Home(R.drawable.avatar, "Nguyen Thanh An","Hôm qua lúc 7:00",R.drawable.img_content));
+        Home_Adapter adapter = new Home_Adapter(getContext(),R.layout.home,arrayList);
+        listview.setAdapter(adapter);
         return view;
     }
 }
