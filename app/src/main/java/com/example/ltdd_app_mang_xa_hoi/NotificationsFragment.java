0ac94dd220394b9796ccd15ded02eb09c5932701
vietdz453 <viethanso453@gmail.com>
2023-10-12 13:00:22 +0700

    làm trang thông báo(fragment_notifications, ThongBao.java,NotificationsFragment.java,NotificationAdapter.java,thongbao.xml), chi tiết bài đăng(chitietbaidang.xml), phản hồi(phanhoi.xml), tìm kiếm(search.xml), thêm vài string, icon và ảnh.

--- app/src/main/java/com/example/ltdd_app_mang_xa_hoi/NotificationsFragment.java
+++ app/src/main/java/com/example/ltdd_app_mang_xa_hoi/NotificationsFragment.java
@@ -7,14 +7,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import Adapters.NotificationAdapter;
+import Dto.ThongBao;
+import Dto.ThongBao;
 
 public class NotificationsFragment extends Fragment {
+    ListView listview;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+        listview = view.findViewById(R.id.listviewnotification);
+        ArrayList<ThongBao> arrayList = new ArrayList<>();
+        arrayList.add(new ThongBao(R.drawable.avatar, "Nguyen Thanh An", "đã thích bài viết của bạn"));
+        arrayList.add(new ThongBao(R.drawable.avatar, "Nguyen Thanh An", "đã bình luận về bài viết của bạn"));
+        arrayList.add(new ThongBao(R.drawable.avatar, "Nguyen Thanh An", "đã chia sẻ bài viết của bạn"));
+        NotificationAdapter adapter = new NotificationAdapter(getContext(), R.layout.thongbao, arrayList);
+        listview.setAdapter(adapter);
+        return view;
     }
 }
